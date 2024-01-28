e7f1710e1674ad2787bbf3632f924e74c681a27a
VyosimMohammedkhan <133080732+VyosimMohammedkhan@users.noreply.github.com>
2024-01-28 17:54:34 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Paslyas app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>my code</code>
         </p>
       </main>
 
